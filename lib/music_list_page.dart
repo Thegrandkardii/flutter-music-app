f9881e65e393fc4e134595070466024141f83c93
Sarah Ajekwe <ajekwesarah@gmail.com>
2024-01-17 15:27:28 +0100

    add files

--- lib/music_list_page.dart
+++ lib/music_list_page.dart
@@ -20,48 +20,54 @@ class MusicListPage extends StatefulWidget {
 }
 
 class _MusicListPageState extends State<MusicListPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<String, Dynamic> item = feedItems[pos]:
 
   @override
   Widget build(BuildContext context) {
     
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-        
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: Color.black,
+        title: "Tracks",
+        style: TextStyle(
+          color: Colors.white, fontSize:16, Fontweight: fontweight.bold
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ),
+      body: Container(
+        child: ListView.separated(
+          itemCount: 8,
+          itemBuilder: (BuildContext context, int position) {}
+            return Container(
+              child: ListTile(
+                leading: ClipReact(
+                  BorderRadius: BorderRadiusDirectional.circular(16),
+                  child: Container(
+                    height:0,
+                    width: 0,
+                    child: Image.asset(assets/image.jpg),
+                    height: 60,
+                    width: 60,
+                    ),
+                  ),
+                ),
+            ),
+                title: Text(
+                  item["Sweater Weather"],
+                style: StyleText(color: Colors.white, fontsize: 24),
+                ),
+                subtitile: Text(
+                  item["The Nieghborhood"],
+                  style: StyleText(color: Colors.white, fontsize: 16)
+                  child: Icon(
+                    Icons.
+
+                  )
+                ),
+              ),
+            );
+
+          },
+        
+        
   }
-}
